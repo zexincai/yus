c730dc0ff7b9eb8d414347ef7e5f795bced902ca
Lion <zexincaicai@163.com>
2025-07-03 17:57:38 +0800

    feat(账号管理): 添加账号注销功能及相关配置
    - 新增账号注销页面及路由配置
    - 在用户信息页添加注销账号入口
    - 添加安卓隐私政策配置文件
    - 更新manifest版本号及微信小程序appid
    - 配置iOS隐私权限描述

--- pages/index/info/index.vue
+++ pages/index/info/index.vue
@@ -37,6 +37,7 @@
     <!-- 退出账号 -->
     <view class="logout-link" @click="handleViewPrivacy">隐私政策>></view>
     <view class="logout-link" style="margin-top: 20rpx;" @click="handleLogout">退出当前账号>></view>
+    <view class="logout-link" style="margin-top: 20rpx;color:#DB4E51" @click="toCancel">注销账号>></view>
   </view>
 </template>
 
@@ -103,6 +104,14 @@ const handleViewPrivacy = () => {
     url: `/pages/login/privacy/index`,
   });
 };
+const toCancel = () => {
+  // TODO: 跳转到隐私政策页面
+  uni.navigateTo({
+    url: `/pages/login/cancel/index`,
+  });
+};
+
+
 const handleLogout = () => {
   // TODO: 退出登录逻辑
   uni.showModal({
